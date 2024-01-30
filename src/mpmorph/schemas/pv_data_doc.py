6c8ef86b99be6ff89aaabcdb55782d38cd3b1b58
Hui Zheng <huz071@eng.ucsd.edu>
2024-01-30 11:54:34 -0800

    debug task_label setter

--- src/mpmorph/schemas/pv_data_doc.py
+++ src/mpmorph/schemas/pv_data_doc.py
@@ -7,15 +7,17 @@ from pydantic import BaseModel, Field
 #     volume: float = Field(None, description="The volume data from the MD run")
 #     pressure: float = Field(None, description="The volume data from the MD run")
 class MDPVDataDoc(BaseModel):
-    _task_label: str = Field(None, description="The name of the task.")
+    task_label: str = Field(None, description="The name of the task.")
+    volume: float = Field(None, description="The volume data from the MD run")
+    pressure: float = Field(None, description="The volume data from the MD run")    
     # other fields...
 
     @property
     def task_label(self):
-        return self._task_label
+        return self.__dict__['task_label']
 
     @task_label.setter
     def task_label(self, value):
         if value is None:
             raise ValueError("task_label should not be None")
-        self._task_label = value
+        self.__dict__['task_label'] = value
