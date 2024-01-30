835e5cc432776d1bc539b80ab1130b36660a7b8e
Hui Zheng <huz071@eng.ucsd.edu>
2024-01-30 11:45:45 -0800

    debug via overriding the setter

--- src/mpmorph/schemas/pv_data_doc.py
+++ src/mpmorph/schemas/pv_data_doc.py
@@ -1,8 +1,21 @@
 from pydantic import BaseModel, Field
 
 
-class MDPVDataDoc(BaseModel):
+# class MDPVDataDoc(BaseModel):
 
-    task_label: str = Field(None, description="The name of the task.")
-    volume: float = Field(None, description="The volume data from the MD run")
-    pressure: float = Field(None, description="The volume data from the MD run")
+#     task_label: str = Field(None, description="The name of the task.")
+#     volume: float = Field(None, description="The volume data from the MD run")
+#     pressure: float = Field(None, description="The volume data from the MD run")
+class MDPVDataDoc(BaseModel):
+    _task_label: str = Field(None, description="The name of the task.")
+    # other fields...
+
+    @property
+    def task_label(self):
+        return self._task_label
+
+    @task_label.setter
+    def task_label(self, value):
+        if value is None:
+            raise ValueError("task_label should not be None")
+        self._task_label = value
